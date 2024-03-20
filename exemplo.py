d0965a599f2c63012c2bc41d30c2168c27a74ccd
nicholasassis <160735364+nicholasassis@users.noreply.github.com>
2024-03-20 09:35:11 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,49 @@
+print ("nicholas")
+
+numero = 11
+nome = "nicholas"
+booleano = True
+
+
+
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicaçao = a * b
+divisao = a / b
+
+
+
+
+numeros = [10, 20, 30, 40]
+frase = "Esta é uma frase de exemplo"
+palavra = "Python"
+
+
+media = sum(numeros) / len(numeros)
+print("Média aritmética dos números:", media)
+
+
+numero_quadrado = numeros[0] ** 2
+print("Quadrado do primeiro número:", numero_quadrado)
+
+
+numero_dobro = numeros[1] * 2
+print("Dobro do segundo número:", numero_dobro)
+
+
+quantidade_letras = len(palavra)
+print("Quantidade de letras na palavra:", quantidade_letras)
+
+
+quantidade_espacos = frase.count(" ")
+print("Quantidade de espaços em branco na frase:", quantidade_espacos)
+
+
+primeiro_maior = numeros[0] > numeros[1]
+print("O primeiro número é maior que o segundo:", primeiro_maior)
+
+
+maior_numero = max(numeros)
+print("Maior número:", maior_numero)
